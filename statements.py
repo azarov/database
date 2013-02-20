3c487d2fc7f40da0092fca728de3e95d60482c71
slava <slava@ISIDA.(none)>
2013-02-20 20:44:57 +0400

    where statement added

--- statements.py
+++ statements.py
@@ -17,15 +17,33 @@ class InsertStatement(object):
 		return "InsertStatement: [tablename: {0}, values: {1}]".format(self.tablename, self.values)
 
 class SelectStatement(object):
-	def __init__(self, tablename):
+	def __init__(self, tablename, whereStmt=None):
 		self.tablename = tablename
+		self.whereStmt = whereStmt
 
 	def __repr__(self):
-		return "SelectStatement: [tablename: {0}]".format(self.tablename)
+		if self.whereStmt != None:
+			return "SelectStatement: [tablename: {0}; {1}]".format(self.tablename, self.whereStmt)
+		else:
+			return "SelectStatement: [tablename: {0}]".format(self.tablename)
 
 class DropStatement(object):
 	def __init__(self, tablename):
 		self.tablename = tablename
 
 	def __repr__(self):
-		return "DropStatement: [tablename: {0}]".format(self.tablename)	
+		return "DropStatement: [tablename: {0}]".format(self.tablename)
+
+class WhereStatement(object):
+	def __init__(self, colname, operation, value):
+		self.colname = colname
+		self.operation = operation
+		self.value = value
+
+	def __repr__(self):
+		return "WhereStatement: [colname: {0}, operation: {1}, value: {2}]".format(self.colname, self.operation, self.value)
+
+def enum(**enums):
+	return type('Enum', (), enums)
+
+WhereOps = enum(EQ="=", NEQ="!=", LT="<", GT=">", LEQ="<=", GEQ=">=")
